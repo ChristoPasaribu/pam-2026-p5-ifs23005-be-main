339ed3e92e1c40d357288990e56f764976c561cb
Christo Sadatua Manatap Pasaribu <christopasaribu90@gmail.com>
2026-03-03 16:18:02 +0700

    update

--- src/main/kotlin/services/UpdateProfileRequest.kt
+++ src/main/kotlin/services/UpdateProfileRequest.kt
@@ -1,7 +1,10 @@
 package org.delcom.services
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class UpdateProfileRequest(
     val name: String,
     val username: String,
-    val about: String?
+    val about: String? = null
 )
